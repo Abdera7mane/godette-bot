8d6a47ec13794cf5eab02a48b47d5230dacb83ce
Abdera7mane <59313921+Abdera7mane@users.noreply.github.com>
2022-10-26 23:10:46 +0100

    Fancier commands

--- src/commands/DocsCommandExecutor.gd
+++ src/commands/DocsCommandExecutor.gd
@@ -1,4 +1,5 @@
 # warning-ignore-all:return_value_discarded
+# warning-ignore-all:integer_division
 
 class_name DocsCommandExecutor extends ApplicationCommandExecutor
 
@@ -117,28 +118,81 @@ func display_class_documentation(
 		embed.add_field("Tutorials", "**" + tutorials.join(", ") + "**")
 	
 	action.add_embed(embed)
+	
+	var row := MessageActionRowBuilder.new()
+	if class_reference.members.size() > 0:
+		row.add_component(
+			MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+			.with_custom_id("property").with_label("Properties")
+		)
+	if class_reference.methods.size() > 0:
+		row.add_component(
+			MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+			.with_custom_id("method").with_label("Methods")
+		)
+	if class_reference.signals.size() > 0:
+		row.add_component(
+			MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+			.with_custom_id("signal").with_label("Signals")
+		)
+	if class_reference.constants.size() > 0:
+		row.add_component(
+			MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+			.with_custom_id("constant").with_label("Constants")
+		)
+	if class_reference.constants.size() > 0:
+		row.add_component(
+			MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+			.with_custom_id("enum").with_label("Enums")
+		)
+	
+	action.add_component(row)
 
 func display_methods_documentation(
-	clazz: String, api: GodotAPIReference, action: MessageAction
-) -> void:
+	clazz: String, api: GodotAPIReference, page: int, action: MessageAction
+) -> int:
 	var class_reference: ClassAPIReference = api.get_class_reference(clazz)
 	var clazz_name: String = class_reference.name
-	if class_reference.methods.size() == 0:
-		action.set_content("**No methods in base `%s`**" % clazz_name)
+	var total_methods: int = class_reference.methods.size()
+	if total_methods == 0:
+		action.set_content("**No methods in base `%s`**" % clazz_name)\
+			.ephemeral(true)
 	else:
 		var methods: Array = class_reference.methods.values()
 		
 		var description: String = "`%s` class methods\n" % clazz_name
-		for i in range(min(methods.size(), 5)):
-			var method: MethodAPIReference = methods[i]
+		var pages: int = total_methods / 5
+		page = int(clamp(page, 0, pages))
+		var offset: int = 5 * page
+		var row := MessageActionRowBuilder.new()
+		for i in range(min(total_methods, min(5, total_methods - offset))):
+			var method: MethodAPIReference = methods[i + offset]
 			# Arduino is the closest to highlight most of the method syntax
 			description += str('```ino\n', method, '```')
+			row.add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id(method.name).with_label(method.name)
+			)
 		
 		action.add_embed(
 			generate_class_embed(clazz_name, "#method-descriptions")\
-			.set_title(clazz_name + " methods")\
-			.set_description(description)
-		)
+			.set_title(clazz_name + " methods").set_description(description)
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("class").with_label("Overview")
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("previous").with_label("Previous")\
+				.disabled(page == 0)
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("next").with_label("Next")\
+				.disabled(page == pages)
+			)
+		).add_component(row)
+		return page
+	return 0
 
 func display_method_documentation(
 	clazz: String, method: String, api: GodotAPIReference,
@@ -154,30 +208,61 @@ func display_method_documentation(
 			.set_title("%s.%s()" % [clazz_name, method_ref.name])\
 			.set_description(method_ref.description)\
 			.add_field("Declaration", str('```ino\n', method_ref, '```'))
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("method").with_label("Methods")
+			)
 		)
 	else:
-		action.set_content("**`%s` method not found in base `%s`**" % [method, clazz_name])
+		action.set_content("**`%s` method not found in base `%s`**" % [
+			method, clazz_name
+		]).ephemeral(true)
 
 func display_properties_documentation(
-	clazz: String, api: GodotAPIReference, action: MessageAction
-) -> void:
+	clazz: String, api: GodotAPIReference, page: int, action: MessageAction
+) -> int:
 	var class_reference: ClassAPIReference = api.get_class_reference(clazz)
 	var clazz_name: String = class_reference.name
-	if class_reference.members.size() == 0:
-		action.set_content("**No properties in base `%s`**" % clazz_name)
+	var total_members: int = class_reference.members.size()
+	if total_members == 0:
+		action.set_content("**No properties in base `%s`**" % clazz_name)\
+			.ephemeral(true)
 	else:
 		var members: Array = class_reference.members.values()
-		
 		var description: String = "`%s` class properties\n" % clazz_name
-		for i in range(min(members.size(), 5)):
-			var member: MemberAPIReference = members[i]
+		var pages: int = total_members / 5
+		page = int(clamp(page, 0, pages))
+		var offset: int = 5 * page
+		var row := MessageActionRowBuilder.new()
+		for i in range(min(total_members, min(5, total_members - offset))):
+			var member: MemberAPIReference = members[i + offset]
 			description += str('```ino\n', member, '```')
+			row.add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id(member.name).with_label(member.name)
+			)
 		
 		action.add_embed(
 			generate_class_embed(clazz_name, "#property-descriptions")\
 			.set_title(clazz_name + " properties")\
 			.set_description(description)
-		)
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("class").with_label("Overview")
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("previous").with_label("Previous")\
+				.disabled(page == 0)
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("next").with_label("Next")\
+				.disabled(page == pages)
+			)
+		).add_component(row)
+		return page
+	return 0
 
 func display_property_documentation(
 	clazz: String, property: String, api: GodotAPIReference,
@@ -193,33 +278,64 @@ func display_property_documentation(
 			.set_title("%s.%s" % [clazz_name, property_ref.name])\
 			.set_description(property_ref.description)\
 			.add_field("Declaration", str('```ino\n', property_ref, '```'))
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("property").with_label("Properties")
+			)
 		)
 	else:
 		action.set_content("**`%s` property not found in base `%s`**" % [
 			property, clazz_name
-		])
+		]).ephemeral(true)
 
 func display_signals_documentation(
-	clazz: String, api: GodotAPIReference, action: MessageAction
-) -> void:
+	clazz: String, api: GodotAPIReference, page: int, action: MessageAction
+) -> int:
 	var class_reference: ClassAPIReference = api.get_class_reference(clazz)
 	var clazz_name: String = class_reference.name
+	var total_signals: int = class_reference.signals.size()
 	if class_reference.signals.size() == 0:
-		action.set_content("**No signals in base `%s`**" % clazz_name)
+		action.set_content("**No signals in base `%s`**" % clazz_name)\
+			.ephemeral(true)
 	else:
 		var signals: Array = class_reference.signals.values()
-		
 		var description: String = "`%s` class signals\n" % clazz_name
-		for i in range(min(signals.size(), 5)):
-			var signal_ref: SignalAPIReference = signals[i]
+		var pages: int = total_signals / 5
+		page = int(clamp(page, 0, pages))
+		var offset: int = 5 * page
+		var row := MessageActionRowBuilder.new()
+		for i in range(min(total_signals, min(5, total_signals - offset))):
+			var signal_ref: SignalAPIReference = signals[i + offset]
 			# Yeah whatever, haskell time
 			description += str('```hs\n', signal_ref, '```')
+			row.add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id(signal_ref.name).with_label(signal_ref.name)
+			)
 		
 		action.add_embed(
 			generate_class_embed(clazz_name, "#signals")\
 			.set_title(clazz_name + " singals")\
 			.set_description(description)
-		)
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("class").with_label("Overview")
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("previous").with_label("Previous")\
+				.disabled(page == 0)
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("next")\
+				.with_label("Next")\
+				.disabled(page == pages)
+			)
+		).add_component(row)
+		return page
+	return 0
+
 
 func display_signal_documentation(
 	clazz: String, signal_name: String, api: GodotAPIReference,
@@ -235,31 +351,62 @@ func display_signal_documentation(
 			.set_title("%s [signal] %s" % [clazz_name, signal_ref.name])\
 			.set_description(signal_ref.description)\
 			.add_field("Declaration", str('```hs\n', signal_ref, '```'))
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("signal").with_label("Signals")
+			)
 		)
 	else:
-		action.set_content("**`%s` signal not found in base `%s`**" % [signal_name, clazz_name])
+		action.set_content("**`%s` signal not found in base `%s`**" % [
+			signal_name, clazz_name
+		]).ephemeral(true)
 
 func display_constants_documentation(
-	clazz: String, api: GodotAPIReference, action: MessageAction
-) -> void:
+	clazz: String, api: GodotAPIReference, page: int, action: MessageAction
+) -> int:
 	var class_reference: ClassAPIReference = api.get_class_reference(clazz)
 	var clazz_name: String = class_reference.name
-	
-	if class_reference.constants.size() == 0:
-		action.set_content("**No constants in base `%s`**" % clazz_name)
+	var total_constants: int = class_reference.constants.size()
+	if total_constants == 0:
+		action.set_content("**No constants in base `%s`**" % clazz_name)\
+			.ephemeral(true)
 	else:
 		var constants: Array = class_reference.constants.values()
-		
 		var description: String = "`%s` class constants\n" % clazz_name
-		for i in range(min(constants.size(), 5)):
-			var constant: ConstantAPIReference = constants[i]
+		var pages: int = total_constants / 5
+		page = int(clamp(page, 0, pages))
+		var offset: int = 5 * page
+		var row := MessageActionRowBuilder.new()
+		for i in range(min(total_constants, min(5, total_constants - offset))):
+			var constant: ConstantAPIReference = constants[i + offset]
 			description += str('```ino\n', constant, '```')
+			row.add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id(constant.name).with_label(constant.name)
+			)
 		
 		action.add_embed(
 			generate_class_embed(clazz_name, "#constants")\
 			.set_title(clazz_name + " constants")\
 			.set_description(description)
-		)
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("class").with_label("Overview")
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("previous").with_label("Previous")\
+				.disabled(page == 0)
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("next").with_label("Next")\
+				.disabled(page == pages)
+			)
+		).add_component(row)
+		return page
+	return 0
+
 
 func display_constant_documentation(
 	clazz: String, constant: String, api: GodotAPIReference,
@@ -275,31 +422,62 @@ func display_constant_documentation(
 			.set_title("%s.%s" % [clazz_name, const_ref.name])\
 			.set_description(const_ref.description)\
 			.add_field("Declaration", str('```hs\n', const_ref, '```'))
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("constant").with_label("Constants")
+			)
 		)
 	else:
-		action.set_content("**`%s` constant not found in base `%s`**" % [constant, clazz_name])
-		action
+		action.set_content("**`%s` constant not found in base `%s`**" % [
+			constant, clazz_name
+		]).ephemeral(true)
 
 func display_enums_documentation(
-	clazz: String, api: GodotAPIReference, action: MessageAction
-) -> void:
+	clazz: String, api: GodotAPIReference, page: int, action: MessageAction
+) -> int:
 	var class_reference: ClassAPIReference = api.get_class_reference(clazz)
 	var clazz_name: String = class_reference.name
+	var total_enums: int = class_reference.enums.size()
 	if class_reference.enums.size() == 0:
-		action.set_content("**No enums in base `%s`**" % clazz_name)
+		action.set_content("**No enums in base `%s`**" % clazz_name)\
+			.ephemeral(true)
 	else:
 		var enums: Array = class_reference.enums.values()
-		
 		var description: String = "`%s` class enums\n" % clazz_name
-		for i in range(min(enums.size(), 5)):
-			var enum_ref: EnumAPIReference = enums[i]
+		var pages: int = total_enums / 5
+		page = int(clamp(page, 0, pages))
+		var offset: int = 5 * page
+		var row := MessageActionRowBuilder.new()
+		for i in range(min(total_enums, min(5, total_enums - offset))):
+			var enum_ref: EnumAPIReference = enums[i + offset]
 			description += str('```swift\n', enum_ref.name, '```')
+			row.add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id(enum_ref.name).with_label(enum_ref.name)
+			)
 		
 		action.add_embed(
 			generate_class_embed(clazz_name, "#enumerations")\
 			.set_title(clazz_name + " enums")\
 			.set_description(description)
-		)
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("class").with_label("Overview")
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("previous").with_label("Previous")\
+				.disabled(page == 0)
+			).add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.SECONDARY)\
+				.with_custom_id("next").with_label("Next")\
+				.disabled(page == pages)
+			)
+		).add_component(row)
+		return page
+	return 0
+
 
 func display_enum_documentation(
 	clazz: String, enum_name: String, api: GodotAPIReference,
@@ -321,14 +499,59 @@ func display_enum_documentation(
 			generate_class_embed(clazz_name, "#enumerations")\
 			.set_title("%s.%s" % [clazz_name, enum_ref.name])\
 			.set_description(description)\
+		).add_component(
+			MessageActionRowBuilder.new().add_component(
+				MessageButtonBuilder.new(MessageButton.Styles.PRIMARY)\
+				.with_custom_id("enum").with_label("Enums")
+			)
 		)
 	else:
-		action.set_content("**`%s` enum not found in base `%s`**" % [enum_name, clazz_name])
+		action.set_content("**`%s` enum not found in base `%s`**" % [
+			enum_name, clazz_name
+		]).ephemeral(true)
+
+func display_attribute(
+	api_reference: GodotAPIReference, clazz: String,
+	attribute: String, attribute_name: String, page: int,
+	action: MessageAction
+) -> int:
+	match attribute:
+		"method":
+			if attribute_name.empty():
+				page = display_methods_documentation(clazz, api_reference, page, action)
+			else:
+				display_method_documentation(clazz, attribute_name, api_reference, action)
+		"property":
+			if attribute_name.empty():
+				page = display_properties_documentation(clazz, api_reference, page, action)
+			else:
+				display_property_documentation(clazz, attribute_name, api_reference, action)
+		"signal":
+			if attribute_name.empty():
+				page = display_signals_documentation(clazz, api_reference, page, action)
+			else:
+				display_signal_documentation(clazz, attribute_name, api_reference, action)
+		"constant":
+			if attribute_name.empty():
+				page = display_constants_documentation(clazz, api_reference, page, action)
+			else:
+				display_constant_documentation(clazz, attribute_name, api_reference, action)
+		"enum":
+			if attribute_name.empty():
+				page = display_enums_documentation(clazz, api_reference, page, action)
+			else:
+				display_enum_documentation(clazz, attribute_name, api_reference, action)
+		_:
+			display_class_documentation(clazz, api_reference, action)
+		
+	yield(action.submit(), "completed")
+	return page
 
 func _on_slash_command(command: DiscordSlashCommand) -> void:
 	var clazz: String = command.get_string_option("class_name")
 	var attribute: String = command.get_string_option("attribute")
 	var attribute_name: String = command.get_string_option("attribute_name")
+	var page: int = command.get_integer_option("page")
 	var for_godot_4: bool = command.get_boolean_option("godot_4")
 	
 #	var api_reference: GodotAPIReference = godot_4 if for_godot_4 else godot_3
@@ -342,35 +565,47 @@ func _on_slash_command(command: DiscordSlashCommand) -> void:
 		)
 	
 	elif api_reference.has_class(clazz):
-		match attribute:
-			"method":
-				if attribute_name.empty():
-					display_methods_documentation(clazz, api_reference, action)
-				else:
-					display_method_documentation(clazz, attribute_name, api_reference, action)
-			"property":
-				if attribute_name.empty():
-					display_properties_documentation(clazz, api_reference, action)
-				else:
-					display_property_documentation(clazz, attribute_name, api_reference, action)
-			"signal":
-				if attribute_name.empty():
-					display_signals_documentation(clazz, api_reference, action)
-				else:
-					display_signal_documentation(clazz, attribute_name, api_reference, action)
-			"constant":
-				if attribute_name.empty():
-					display_constants_documentation(clazz, api_reference, action)
-				else:
-					display_constant_documentation(clazz, attribute_name, api_reference, action)
-			"enum":
-				if attribute_name.empty():
-					display_enums_documentation(clazz, api_reference, action)
-				else:
-					display_enum_documentation(clazz, attribute_name, api_reference, action)
-			_:
-				display_class_documentation(clazz, api_reference, action)
+		yield(display_attribute(
+			api_reference, clazz, attribute,
+			attribute_name, page, action
+		), "completed")
 	else:
-		action.set_content("**Class name `%s` not found**" % clazz)
+		action.set_content("**Class name `%s` not found**" % clazz)\
+			.ephemeral(true).submit()
 	
-	action.submit()
+	
+	var response: Message = yield(command.fetch_response(), "completed")
+	if not response:
+		return
+
+	var awaiter := await_components(response.id, 30_000)
+	
+	var previous: String
+	while yield(awaiter.wait(), "completed"):
+		awaiter.reset()
+		var event := awaiter.get_event()
+		var custom_id: String = event.data.custom_id
+		action = event.update_message()
+		match custom_id:
+			"next":
+				page += 1
+			"previous":
+				page -= 1
+			"class":
+				attribute = ""
+				attribute_name = ""
+			"method", "property", "signal", "constant", "enum":
+				if previous != custom_id:
+					page = 0
+				attribute = custom_id
+				attribute_name = ""
+				previous = attribute
+			_:
+				attribute_name = custom_id
+		
+		page = yield(display_attribute(
+			api_reference, clazz, attribute,
+			attribute_name, page, action
+		), "completed")
+	
+	command.edit_response().clear_components().submit()
--- src/commands/ExpressionCommandExecutor.gd
+++ src/commands/ExpressionCommandExecutor.gd
@@ -10,7 +10,7 @@ func _on_slash_command(command: DiscordSlashCommand) -> void:
 	var expression_string: String = command.get_string_option("expression")
 	if expression.parse(expression_string, singletons.keys()) != OK:
 		var reply: String = "```diff\n"
-		reply += "-" + expression.get_error_text()
+		reply += "- " + expression.get_error_text()
 		reply += "\n```"
 		command.create_reply(reply).submit()
 		return
@@ -19,14 +19,18 @@ func _on_slash_command(command: DiscordSlashCommand) -> void:
 	var result: String = str(expression.execute(singletons.values(), null, false))
 	if expression.has_execute_failed():
 		formatting = "diff"
-		result = "-" + expression.get_error_text()
+		result = "- " + expression.get_error_text()
+	elif result.length() > MessageAction.MAX_CONTENT_LENGTH - 7 - formatting.length():
+		formatting = "diff"
+		result = "- " + "Output is too big"
 	
 	if result.empty():
 		formatting = "diff"
-		result = "! Empty"
+		result = "! Empty output"
+	
 	
 	command.create_reply(
 		"```" + formatting + "\n"
 		+ result
 		+ "```"
-	).submit()
+	).ephemeral(true).submit()
